8d1c9bf94a07b91f6f02a2628924b77520d1f362
Les Hill <leshill@gmail.com>
2011-04-08 14:39:50 -0700

    with_resque now uses #inline

--- lib/resque_spec/helpers.rb
+++ lib/resque_spec/helpers.rb
@@ -2,26 +2,11 @@ module ResqueSpec
   module Helpers
 
     def with_resque
-      enable_perform
-      yield
-      disable_perform
-    end
-
-    private
-
-    def enable_perform
-      ::Resque.module_eval do
-        def self.enqueue(klass, *args)
-          klass.perform(*args)
-        end
-      end
-    end
-
-    def disable_perform
-      ::Resque.module_eval do
-        def self.enqueue(klass, *args)
-          ::Resque::Job.create(ResqueSpec.queue_name(klass), klass, *args)
-        end
+      begin
+        ResqueSpec.inline = true
+        yield
+      ensure
+        ResqueSpec.inline = false
       end
     end
   end

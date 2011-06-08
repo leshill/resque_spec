6a57cf91aaea04ba93669e636f03472d1608d684
Dennis Walters <dennis@canvashosting.com>
2011-06-08 18:38:09 -0400

    Added wrapper for Resque.enqueue_in
    As provided by ResqueScheduler, this enqueues a job by a relative
    time rather than an absolute time, a-la:
    
      Resque.enqueue_in(1.hour, SomeJob, 'an argument')

--- lib/resque_spec/scheduler.rb
+++ lib/resque_spec/scheduler.rb
@@ -5,10 +5,18 @@ module ResqueSpec
     def enqueue_at(time, klass, *args)
       ResqueSpec.enqueue_at(time, klass, *args)
     end
+
+    def enqueue_in(time, klass, *args)
+      ResqueSpec.enqueue_in(time, klass, *args)
+    end
   end
 
   def enqueue_at(time, klass, *args)
     store(schedule_queue_name(klass), klass, { :class => klass.to_s, :time  => time, :args => args })
+  end
+
+  def enqueue_in(time, klass, *args)
+    enqueue_at(Time.now + time, klass, *args)
   end
 
   def schedule_for(klass)

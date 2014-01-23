f6b971ce70e2ab7b9a5a6c9b6a57f6ad6147e811
Les Hill <leshill@gmail.com>
2014-01-22 21:19:20 -0800

    Allow should syntax with RSpec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,11 @@ require 'pry'
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
